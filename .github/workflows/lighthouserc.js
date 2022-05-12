1a20fca08e7210b3c77ab8e40ee4f3e4167b1292
Bruno <bruno.firmiano@acct.global>
2022-05-12 16:57:51 -0300

    Correction configuration

--- .github/workflows/lighthouserc.js
+++ .github/workflows/lighthouserc.js
@@ -1,40 +1,16 @@
 module.exports = {
     ci: {
-      server: {
-        psiCollectCron: {
-          psiApiKey: process.env.PSI_API_KEY,
-          sites: [
-            {
-              label: 'Production',
-              projectSlug: 'the-project',
-              schedule: '0 * * * *', // at the top of the hour, every hour
-              numberOfRuns: 5,
-              urls: ['http://example.com', 'http://example.com/pricing'],
-            },
-            {
-              label: 'Development',
-              branch: 'dev',
-              projectSlug: 'the-project',
-              schedule: '*/10 * * * *', // every 10 minutes
-              numberOfRuns: 3,
-              urls: ['http://staging.example.com', 'http://staging.example.com/pricing'],
-            },
-          ],
+        assert: {
+          preset: "lighthouse:recommended",
+          assertions: {
+            "offscreen-images": "off",
+            "uses-webp-images": "off",
+            "color-contrast": "off"
+            // ...
+          }
         },
-        storage: {
-          storageMethod: 'sql',
-          sqlDialect: 'postgres',
-          sqlConnectionSsl: true,
-          sqlConnectionUrl: process.env.DATABASE_URL,
-          sqlDialectOptions: {
-            ssl: true,
-          },
-          sequelizeOptions: {
-            pool: {
-              acquire: 30000,
-            },
-          },
-        },
-      },
-    },
+        upload: {
+          target: "temporary-public-storage"
+        }
+      }
   };
